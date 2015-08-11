b7fd5e6515753ed7143da150f574469771390765
Peter Bright <drpizza@quiscalusmexicanus.org>
2015-08-11 01:46:33 -0400

    Revert "Revert "Switch to using TBB instead of ConcRT, to open the door to cross-platform ports.""
    This reverts commit 15155227cae34981a845681dfb9af9e8b034dd50.

--- benchmark/src/benchmark.cpp
+++ benchmark/src/benchmark.cpp
@@ -1,5 +1,7 @@
 // copyright 2015 Peter Bright. See LICENSE.txt for licensing details.
 
+#define NOMINMAX
+
 #include <SDKDDKVer.h>
 
 #include "encoder.hpp"
@@ -11,11 +13,11 @@
 
 int main()
 {
-	static constexpr uint8_t DATA_COUNT = 17;
-	static constexpr uint8_t PARITY_COUNT = 3;
+	static constexpr uint8_t DATA_COUNT = 16;
+	static constexpr uint8_t PARITY_COUNT = 4;
 	static constexpr uint8_t TOTAL_COUNT = DATA_COUNT + PARITY_COUNT;
-	static constexpr size_t BUFFER_SIZE = 200 * 1024;
-	static constexpr size_t NUMBER_OF_BUFFER_SETS = 10;
+	static constexpr size_t BUFFER_SIZE = 16 * 1024 * 1024;
+	static constexpr size_t NUMBER_OF_BUFFER_SETS = 1;
 	static constexpr std::chrono::seconds MEASUREMENT_DURATION{ 10 };
 
 	struct buffer_set
